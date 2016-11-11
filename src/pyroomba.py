940229b335ee673239ea76bf1135048b41a80c6b
Alexis CARE <alx.care@gmail.com>
2016-11-11 16:01:48 -0500

    Added getDataStream() fuction; to be tested

--- src/pyroomba.py
+++ src/pyroomba.py
@@ -20,7 +20,42 @@ class Roomba:
     MODE_PASSIVE    = 1
     MODE_SAFE       = 2
     MODE_FULL       = 3
-    
+
+    SENSOR_DATA = {}
+
+    CMD_TO_OCTET    = {
+                        7 : 1,  #Bumps and Wheel Drops
+                        8 : 1,  #Wall
+                        9 : 1,  #Cliff Left
+                        10 : 1, #Cliff Front Left
+                        11 : 1, #Cliff Front Right
+                        12 : 1, #Cliff Right
+                        13 : 1, #Virtual Wall
+                        14 : 1, #Wheel Overcurrents
+                        17 : 1, #Infrared CharacterOmni
+                        52 : 1, #Infrared Character Left
+                        53 : 1, #Infrared Character Right
+                        18 : 1, #Buttons
+                        19 : 2, #Distance
+                        20 : 2, #Angle
+                        35 : 1, #OI Mode
+                        38 : 1, #Number of Stream Packets
+                        39 : 2, #Requested Velocity
+                        40 : 2, #Requested Radius
+                        41 : 2, #Requested Right Velocity
+                        42 : 2, #Requested Left Velocity
+                        43 : 2, #LeftEncoder Counts
+                        44 : 2, #RightEncoder Counts
+                        45 : 1, #Light Bumper
+                        # Groupe commande 106 (46 - 51)
+                        46 : 2, #Light Bump Left Signal
+                        47 : 2, #Light Bump Front Left Signal
+                        48 : 2, #Light Bump Center Left Signal
+                        49 : 2, #Light Bump Center Right Signal
+                        50 : 2, #Light Bump Front Right Signal
+                        51 : 2 #Light Bump Right Signal
+                      }
+
     def __init__(self, port, baudrate):
         self.s = None
         self.connect(port, baudrate)
@@ -48,7 +83,7 @@ class Roomba:
 
     def passiveMode(self):
         self.send([self.CMD_START])
-        
+
     def safeMode(self):
         self.send([self.CMD_MODE_SAFE])
 
@@ -74,16 +109,32 @@ class Roomba:
         data.extend(packetIds)
         self.send(data)
 
-    
+    def getDataSTream(self):
+        n = ord(r.receive(1))
+        trame = self.receive(n+1)
+
+        checksum = (19 + n + sum(bytearray(trame))) & 0xFF
+        if checksum != 0:
+            i = 0;
+            while i < n :
+                    cmd = data[i]
+                    octets = self.CMD_TO_OCTET[cmd]
+                    data = ord(trame[i:i+octets])
+                    self.SENSOR_DATA[cmd] = data
+                    i += 1 + octets
+        else:
+            print "trame corronpue"
+
+
 r = Roomba('COM24', 115200)
 try:
     r.fullMode()
-    
+
     print "mode : {0}".format(r.getMode())
 
     #r.setSong(1, [61,50,62,50,63,50])
     #r.playSong(1)
-    
+
     r.setStream([r.PKT_ID_MODE])
     while True:
         if ord(r.receive(1)) == 19:
@@ -96,13 +147,10 @@ try:
             if checksum != 0:
                 print '[bad CRC : {0}]'.format(checksum),
             print '\n\r'
-                
-    
+
+
     r.disconnect()
-    
+
 except Exception as e:
     print "Catch exception : {0}".format(str(e))
     r.disconnect()
-
-
-
